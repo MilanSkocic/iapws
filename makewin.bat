dae11a25a7b2de846ab3f83a54eed8085182fbc1
Milan Skocic <milan.skocic@gmail.com>
2021-04-22 10:31:04 +0200

    Preparing bat scripts for switching from nmake.

---
+++ makewin.bat
@@ -0,0 +1,69 @@
+@echo off
+setlocal enabledelayedexpansion enableextensions
+
+set cc_intel=icl.exe
+set cc_msvc=cl.exe
+set linker_msvc=link.exe
+set linker_intel=xilink.exe
+set liblinker=lib.exe
+
+:: modify according to your system configuration
+set arch=x64
+set useintel=1
+
+:: Set folders
+set SRC=./src/
+set BIN=./bin/
+set BUILD=./build/
+
+if %useintel% == 1 (
+    set compiler=%cc_intel%
+    set linker=%linker_intel%
+    set cflags=/O3
+    set lflags=
+) else (
+    set compiler=%cc_msvc%
+    set linker=%linker_msvc%
+    set cflags=/O2
+    set lflags=
+)
+
+set EXE=iapws_%COMPILER%_win_%arch%.exe
+
+if "%1" == "" goto all
+
+if "%1" == "all" goto all
+
+if "%1" == "clean" goto clean
+
+if "%1" == "cleanall" goto cleanall
+
+:: compile only one file
+echo "Compiling... "%1"
+%compiler% %cflags% /c %SRC%%1.c /Fo%BUILD%%1.obj
+goto exit
+
+:: compile all files
+:all
+for %%f in (%SRC%*.c) do (
+    echo "Compiling...  %%f"
+    %compiler% %cflags% /c %SRC%%%~nf.c /Fo%BUILD%%%~nf.obj
+)
+:: Link
+set objs=
+for %%f in (%BUILD%*.obj) do (
+    set objs=!objs! %BUILD%%%f
+)
+%linker% %lflags% %objs% /out:%BIN%%EXE%
+goto exit
+
+:clean
+rm -rf %BUILD%*.obj
+goto exit
+
+:cleanall
+rm -rf %BUILD%*.obj
+rm -rf %BIN%*cl*.exe
+goto exit
+
+:exit
