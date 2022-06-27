2e4922ff3ccef0eef1fa114cd73f073a0f750828
Milan Skocic <milan.skocic@gmail.com>
2022-06-27 09:02:51 +0200

    TST: Add tests.

---
+++ tests/test_solubility.c
@@ -0,0 +1,30 @@
+/**
+ * @file test.c
+ * @brief Test IAPWS
+ * 
+ * @details Test water and heavywater formulas
+ * 
+ * Author: Milan Skocic <milan.skocic@gmail.com>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "iapws.h"
+
+
+
+int main(int argc, char **argv){
+    
+    int res = 0;
+    res = test_water();
+
+    res = test_heavywater();
+    
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
