17961689f754c3332e5c2c5aab0714d4262f2737
Milan Skocic <milan.skocic@icloud.com>
2023-03-02 11:30:23 +0100

    ENH: Change Molar volume constant.

--- src/iapws_constants.f90
+++ src/iapws_constants.f90
@@ -36,7 +36,7 @@ real(real64), parameter :: M_D2 = (2*M_D)
 
 real(real64), parameter :: M_water = (M_H*2+M_O) 
 real(real64), parameter :: M_heavywater = (M_D*2+M_O)
-!> Molar volume of ideal gas (273.15 K, 101.325 kPa) in cm3/mol 
-real(real64), parameter ::  Vm = 022413.96954d0 
+!> Molar volume of ideal gas (273.15 K, 100 kPa) in cm3/mol 
+real(real64), parameter ::  Vm_1b = 22710.95464d0 
 
 end module
