2038e10e0392bddb9166b6155114ccf4e65cc970
Milan Skocic <milan.skocic@icloud.com>
2023-03-02 15:46:57 +0100

    ENH: Add solubilties in examples.

--- examples/example_in_c.c
+++ examples/example_in_c.c
@@ -4,18 +4,21 @@
 
 int main(int argc, char **argv){
 
-    double T = 300.0-273.15; /* in C*/
-    char *gas = "He";
+    double T = 25.0; /* in C*/
+    char *gas = "O2";
     char *solvent = "H2O";
-    double kh;
-    int status;
+    double kh, Scm3, Sppm;
     
     if(argc > 1 ){
         printf("%s\n", argv[1]);
     }
 
-    iapws_capi_kh(T, gas, solvent, &kh, &status, strlen(gas), strlen(solvent));
-    printf("Gas=%s\tT=%f°C\tkh=%f\n", gas, T, kh);
+    kh = iapws_capi_kh(T, gas, solvent, strlen(gas), strlen(solvent));
+    Scm3 = iapws_capi_scm3(T, gas, solvent, strlen(gas), strlen(solvent));
+    Sppm = iapws_capi_sppm(T, gas, solvent, strlen(gas), strlen(solvent));
+    printf("Gas=%s\tT=%f°C\tkh=%+10.4f\n", gas, T, kh);
+    printf("Gas=%s\tT=%f°C\tS=%+10.4f cm3.kg-1.bar-1\n", gas, T, Scm3);
+    printf("Gas=%s\tT=%f°C\tS=%+10.4f ppm.bar-1\n", gas, T, Sppm);
     
     return 0;
 }
--- examples/example_in_f.f90
+++ examples/example_in_f.f90
@@ -2,13 +2,17 @@ program example_in_f
     use iso_fortran_env
     use iapws
     implicit none
-    real(real64) :: kh
+    real(real64) :: kh, Scm3, Sppm
     integer(int32) :: status
-    character(len=5) :: gas = "He"
+    character(len=5) :: gas = "O2"
     character(len=5) :: solvent = "H2O"
-    real(real64) :: T = 26.85d0
+    real(real64) :: T = 25.0d0
 
-    call iapws_kh(T, gas, solvent, kh, status)
-    print *, "Gas=", gas, "T=", T, "°C", "kh=", kh
+    kh = iapws_kh(T, gas, solvent)
+    print "(A10, X, A10, X, A2, F10.1, A, 4X, A3, SP, F10.4)", "Gas=", gas, "T=", T, "C", "kh=", kh
+    Scm3 = iapws_scm3(T, gas, solvent)
+    print "(A10, X, A10, X, A2, F10.1, A, 4X, A3, SP, F10.4, A20)", "Gas=", gas, "T=", T, "C", "kh=", Scm3, " cm3.kg-1.bar-1"
+    Sppm = iapws_sppm(T, gas, solvent)
+    print "(A10, X, A10, X, A2, F10.1, A, 4X, A3, SP, F10.4, A20)", "Gas=", gas, "T=", T, "C", "kh=", Sppm, " ppm.bar-1"
 
 end program
