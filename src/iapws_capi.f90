78f16719e0a4f24a997361e22dd68c2f2cd01c38
Milan Skocic <milan.skocic@icloud.com>
2023-03-07 07:17:38 +0100

    ENH: Add temperature scan for kH.

--- src/iapws_capi.f90
+++ src/iapws_capi.f90
@@ -21,6 +21,7 @@ contains
 !! @param[in] size_solvent Length of the string gas.
 !! @return kh Henry constante in mole fraction per GPa. NaN if gas not found.
 function iapws_capi_kh(T, gas, solvent, size_gas, size_solvent)bind(C)result(kh)
+    implicit none
     !! arguments
     real(c_double), value :: T
     type(c_ptr), intent(in), value :: gas
@@ -60,8 +61,9 @@ end function
 !! @param[in] size_solvent Length of the string gas.
 !! @return Scm3 Solubility constant in cm3.kg-1.bar-1. NaN if gas not found.
 function iapws_capi_scm3(T, gas, solvent, size_gas, size_solvent)bind(C)result(Scm3)
+    implicit none
     !! arguments
-    real(c_double), value :: T
+    real(c_double), intent(in), value :: T
     type(c_ptr), intent(in), value :: gas
     type(c_ptr), intent(in), value :: solvent 
     integer(c_int), intent(in), value :: size_gas
@@ -99,8 +101,9 @@ end function
 !! @param[in] size_solvent Length of the string gas.
 !! @return Sppm Solubility constant in ppm. NaN if gas not found.
 function iapws_capi_sppm(T, gas, solvent, size_gas, size_solvent)bind(C)result(Sppm)
+    implicit none
     !! arguments
-    real(c_double), value :: T
+    real(c_double), intent(in), value :: T
     type(c_ptr), intent(in), value :: gas
     type(c_ptr), intent(in), value :: solvent 
     integer(c_int), intent(in), value :: size_gas
@@ -129,4 +132,46 @@ function iapws_capi_sppm(T, gas, solvent, size_gas, size_solvent)bind(C)result(S
     Sppm = iapws_Sppm(T, f_gas, f_solvent)
 
 end function
+
+!> @brief Compute the henry constant for a given temperature and gas in solvent 
+!! @param[in] T Temperature in °C.
+!! @param[in] gas Gas.
+!! @param[in] solvent Solvents: H2O or D2O. Default is H2O.
+!! @param[in] size_gas Length of the string gas.
+!! @param[in] size_solvent Length of the string gas.
+!! @param[out] kh Henry constante in mole fraction per GPa. NaN if gas not found.
+subroutine iapws_capi_kh_vs_T(T, gas, solvent, kh, size_T, size_gas, size_solvent)bind(C)
+    implicit none
+    !! arguments
+    real(c_double), dimension(size_T), intent(in) :: T
+    type(c_ptr), intent(in), value :: gas
+    type(c_ptr), intent(in), value :: solvent 
+    integer(c_int), intent(in), value :: size_T
+    integer(c_int), intent(in), value :: size_gas
+    integer(c_int), intent(in), value :: size_solvent
+    !! returns
+    real(c_double), dimension(size_T), intent(out) :: kh
+    
+    !! local variables
+    character, pointer, dimension(:) :: c2f_gas
+    character, pointer, dimension(:) :: c2f_solvent
+    character(len=size_gas) :: f_gas
+    character(len=size_solvent) :: f_solvent
+    integer(int32) :: i
+
+    call c_f_pointer(gas, c2f_gas, shape=[size_gas])
+    call c_f_pointer(solvent, c2f_solvent, shape=[size_solvent])
+
+    do i=1, size_gas
+        f_gas(i:i) = c2f_gas(i)
+    enddo
+    
+    do i=1, size_solvent
+        f_solvent(i:i) = c2f_solvent(i)
+    enddo
+
+    call iapws_kh_vs_t(T, f_gas, f_solvent, kh)
+
+end subroutine
+
 end module
