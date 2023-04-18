abc7168198591ed3d326a677eed4f6581d541ac5
Milan Skocic <milan.skocic@icloud.com>
2023-04-18 14:40:52 +0200

    ENH: Simplify imports statements.

--- pywrapper/pyiapws/__init__.py
+++ pywrapper/pyiapws/__init__.py
@@ -1,8 +1,6 @@
 """Python wrapper of the (Modern Fortran) iapws library."""
 import platform
 import os
-import ctypes
-
 
 if platform.system() == "Windows":
     arch = None
@@ -11,12 +9,8 @@ if platform.system() == "Windows":
     arch = platform.architecture()[0]
     if arch == "64bit":
         os.add_dll_directory(dll_folder_64bit)
-        ctypes.CDLL(os.path.join(dll_folder_64bit, "libmmd.dll"))
-        ctypes.CDLL(os.path.join(dll_folder_64bit, "libifcoremdd.dll"))
     elif arch == "32bit":
         os.add_dll_directory(dll_folder_32bit)
-        ctypes.CDLL(os.path.join(dll_folder_32bit, "libmmd.dll"))
-        ctypes.CDLL(os.path.join(dll_folder_32bit, "libifcoremdd.dll"))
     else:
         print(f"Error: Architecture was not properly detected:  arch={arch:s}")
         print("Error: dll dependencies cannot be loaded.")
@@ -24,5 +18,3 @@ if platform.system() == "Windows":
 
 from .version import *
 from .iapws import *
-
-from .iapws import get_kh
