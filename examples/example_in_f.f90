64ed3b49e2d120c608743e5b8de49e86b5cdb0c6
Milan Skocic <milan.skocic@icloud.com>
2023-04-21 09:05:49 +0200

    ENH: Remove unused variables.

--- examples/example_in_f.f90
+++ examples/example_in_f.f90
@@ -2,13 +2,12 @@ program example_in_f
     use iso_fortran_env
     use iapws
     implicit none
-    real(real64) :: kh, Scm3, Sppm
-    integer(int32) :: status
+    real(real64) :: kh
     character(len=5) :: gas = "O2"
     character(len=5) :: solvent = "H2O"
     real(real64) :: T = 25.0d0
 
     kh = iapws_kh(T, gas, solvent)
-    print "(A10, X, A10, 1X, A2, F10.1, A, 4X, A3, SP, F10.4)", "Gas=", gas, "T=", T, "C", "kh=", kh
+    print "(A10, 1X, A10, 1X, A2, F10.1, A, 4X, A3, SP, F10.4)", "Gas=", gas, "T=", T, "C", "kh=", kh
 
 end program
