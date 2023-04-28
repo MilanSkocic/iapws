a94188da4bb293c0d1200d90c93ae9365f4af7fc
Milan Skocic <milan.skocic@icloud.com>
2023-04-28 18:20:13 +0200

    ENH: Improve python example.

--- examples/example_in_py.py
+++ examples/example_in_py.py
@@ -2,14 +2,6 @@ r"""Example in python"""
 import numpy as np
 import pyiapws
 
-# int input
-T = int(25)
-pyiapws.kh(T, "O2", "H2O")
-
-# float input
 T = 25.0
 pyiapws.kh(T, "O2", "H2O")
-
-# array input
-T = np.linspace(25.0, 360.0, 10)
-pyiapws.kh(T, "O2", "H2O")
+pyiapws.kd(T, "O2", "H2O")
