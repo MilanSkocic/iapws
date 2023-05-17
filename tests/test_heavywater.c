eba2d9e616b4f3eac5a6b1ede63e2f2a158d27b5
Milan Skocic <milan.skocic@icloud.com>
2023-05-17 14:48:43 +0200

    TST: Add test for kd in D2O.

--- tests/test_heavywater.c
+++ tests/test_heavywater.c
@@ -30,6 +30,17 @@ static double ref_kh[7][4] = {{2.5756, 2.1215, 1.2748, -0.0034},
                             {1.6594, 1.6762, 0.9042, -0.3665},
                             {1.3624, 1.7968, 1.0491, -0.2186}};
 
+// data copied directly from PDF of the paper
+// Guideline on the Henry’s Constant and Vapor-Liquid Distribution Constant for Gases
+// in H2O and D2O at High Temperatures » IAPWS, Kyoto, Japan, G7-04, 2004
+static double ref_kd[7][4] = {{15.2802, 10.4217,7.0674, 3.9539},
+                              {15.1473, 10.5331, 7.3435, 4.2800},
+                              {14.0517, 10.0632, 6.9498,3.9094},
+                              {13.5042, 9.7854, 6.8035, 3.8160},
+                              {12.9782, 9.4648, 6.3074, 3.1402},
+                              {14.3520, 10.0178, 6.6975, 3.5590},
+                              {14.0646, 10.1013, 6.9021, 3.8126}};
+
 static char *gases[] = {"He", "Ne", "Ar", "Kr", "Xe", "D2", "CH4"};
 static double T_K[4] = {300.0, 400.0, 500.0, 600.0};
 
@@ -41,6 +52,7 @@ int main(int argc, char **argv){
 
     double T_C;
     double kh = 0.0;
+    double kd = 0.0;
     char solvent[] = "D2O";
     double diff;
     int i, j;
@@ -62,6 +74,28 @@ int main(int argc, char **argv){
             kh = iapws_capi_kh(T_C, gases[j], solvent, strlen(gases[j]), strlen(solvent))/1000.0;
             diff = roundn(log(kh) - ref_kh[j][i], 4);
             printf("%+7.4f/%+7.4f/%+7.4f\t", log(kh), ref_kh[j][i], log(kh) - ref_kh[j][i]);
+            if(diff != 0.0){
+                return 1;
+            }
+        }
+        printf("\n");
+    }
+    
+    printf("***** Test kd in heavywater *****\n");
+
+    printf("%5s\t", "Gas");
+    for(i=0; i<nT; i++){
+        printf("%23.0f\t", T_K[i]);
+    }
+    printf("\n");
+
+    for(j=0; j<ngas; j++){
+        printf("%5s\t", gases[j]);
+        for(i=0;i<4;i++){
+            T_C = T_K[i] - 273.15;
+            kd = iapws_capi_kd(T_C, gases[j], solvent, strlen(gases[j]), strlen(solvent));
+            diff = roundn(log(kd) - ref_kd[j][i], 4);
+            printf("%+7.4f/%+7.4f/%+7.4f\t", log(kd), ref_kd[j][i], log(kd) - ref_kd[j][i]);
             if(diff != 0.0){
                 return 1;
             }
