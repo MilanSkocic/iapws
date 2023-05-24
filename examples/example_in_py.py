a4dc374313ffae56bac98003c70f5bf1b9812de1
Milan Skocic <milan.skocic@icloud.com>
2023-05-24 11:23:21 +0200

    ENH: Add example for kd in python.

--- examples/example_in_py.py
+++ examples/example_in_py.py
@@ -3,10 +3,12 @@ import array
 import pyiapws
 
 gas  = "O2"
-
 T = array.array("d", (25.0,))
+
 m = pyiapws.g704.kh(T, "O2", 0)
 k = array.array("d", m)
 print(f"Gas={gas}\tT={T[0]}C\tkh={k[0]:+10.4f}\n")
 
-k = pyiapws.g704.kd(T, "O2", 0)
+m = pyiapws.g704.kd(T, "O2", 0)
+k = array.array("d", m)
+print(f"Gas={gas}\tT={T[0]}C\tkh={k[0]:+10.4f}\n")
