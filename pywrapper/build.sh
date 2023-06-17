1fbd77820984ad0e1939568b91b70263aaf8f0ad
Milan Skocic <milan.skocic@icloud.com>
2023-06-17 08:27:11 +0200

    BLD: Minor chnages in build scritps for pywrapper.

--- pywrapper/build.sh
+++ pywrapper/build.sh
@@ -14,19 +14,16 @@ rm ./pyiapws/g704*.so
 
 py311 setup.py sdist
 
-py37 setup.py build_ext --inplace
 py38 setup.py build_ext --inplace
 py39 setup.py build_ext --inplace
 py310 setup.py build_ext --inplace
 py311 setup.py build_ext --inplace
 
-py37 setup.py bdist_wheel
 py38 setup.py bdist_wheel
 py39 setup.py bdist_wheel
 py310 setup.py bdist_wheel
 py311 setup.py bdist_wheel
 
-py37 -m unittest -v
 py38 -m unittest -v
 py39 -m unittest -v
 py310 -m unittest -v
