c9ce9a1e5079930a44866959c68de21ac7133f1b
Milan Skocic <milan.skocic@icloud.com>
2023-09-10 08:36:37 +0200

    ENH: Continue implementation of r797 region 1.

--- src/iapws_r797.f90
+++ src/iapws_r797.f90
@@ -5,8 +5,11 @@ module iapws__r797
     implicit none
     private
 
+    public :: iapws_r797_v
+
     
 !! Parameters from IAPWS R7-97
+real(real64), parameter :: T_KELVIN = 273.15d0
 !! critical temperature of water in K
 real(real64), parameter ::  Tc = 647.096d0 
 !! critical pressure of the water in MPa
@@ -15,6 +18,9 @@ real(real64), parameter ::  pc = 22.064d0
 real(real64), parameter :: rhoc = 322.0d0
 !! Specific gas constant 0.461 526 kJ.kg-1.K-1
 real(real64), parameter :: R = 0.461526d0
+    
+real(real64), parameter :: r1_ps = 16.53d0
+real(real64), parameter :: r1_ts = 1386.0d0
 
 !! IJn_r1f
 real(real64) :: IJn_r1f(1, 3) = transpose(reshape(&
@@ -23,6 +29,51 @@ real(real64) :: IJn_r1f(1, 3) = transpose(reshape(&
 
 contains
 
+pure elemental function gamma_(P, T)result(res)
+    implicit none
+
+    real(real64), intent(in) :: P
+    real(real64), intent(in) :: T
+    
+    real(real64) :: pi
+    real(real64) :: tau
+    real(real64) :: res
+
+    pi = (T+T_KELVIN)/r1_ts
+    tau = P/r1_ps
+    
+    res = sum(IJn_r1f(:,3) * (7.1d0-pi)**IJn_r1f(:,1) * (tau-1.222d0)**IJn_r1f(:, 2))
+
+end function
+
+pure elemental function gamma_pi(P, T)result(res)
+    implicit none
+    
+    real(real64), intent(in) :: P
+    real(real64), intent(in) :: T
+    
+    real(real64) :: pi
+    real(real64) :: tau
+    real(real64) :: res
+
+    pi = (T+T_KELVIN)/r1_ts
+    tau = P/r1_ps
+
+    res = sum(IJn_r1f(:,3) * IJn_r1f(:,1) * (7.1d0-pi)**(IJn_r1f(:,1)-1.0d0) * (tau-1.222d0)**IJn_r1f(:, 2))
+
+end function
+
+pure elemental function iapws_r797_v(P, T)result(res)
+    implicit none
+    
+    real(real64), intent(in) :: P
+    real(real64), intent(in) :: T
+
+    real(real64) :: res
+    
+    res = R*(T+T_KELVIN)/P * gamma_pi(P,T)
+    
+end function
 
 
 end module
