95eb7acf4f507245307be7bdbd07d74daea8c818
Milan Skocic <milan.skocic@icloud.com>
2023-09-22 07:04:24 +0200

    BLD: Switch to pyproject.toml

--- pywrapper/setup.py
+++ pywrapper/setup.py
@@ -3,12 +3,7 @@ import pathlib
 import importlib
 import platform
 import site
-from setuptools import setup, find_packages, Extension
-
-# Import only version.py file for extracting the version
-spec = importlib.util.spec_from_file_location('version', './pyiapws/version.py')
-mod = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(mod)
+from setuptools import setup, Extension
 
 libraries = None
 library_dirs = None
@@ -35,30 +30,5 @@ if __name__ == "__main__":
                         runtime_library_dirs=runtime_library_dirs,
                         extra_objects=extra_objects)
     
-    setup(name=mod.__package_name__,
-        version=mod.__version__,
-        maintainer=mod.__maintainer__,
-        maintainer_email=mod.__maintainer_email__,
-        author=mod.__author__,
-        author_email=mod.__author_email__,
-        description=mod.__package_name__,
-        long_description=pathlib.Path("README.rst").read_text(encoding="utf-8"),
-        long_description_content_type="text/x-rst",
-        url='https://milanskocic.github.io/pyiapws/index.html',
-        download_url='https://github.com/MilanSkocic/pyiapws',
-        packages=find_packages(),
-        include_package_data=False,
-        package_data={"pyiapws": ["libiapws.*"]},
-        python_requires='>=3.8',
-        install_requires=pathlib.Path("requirements.txt").read_text(encoding="utf-8").split('\n'),
-        classifiers=["Development Status :: 5 - Production/Stable",
-                    "Intended Audience :: Science/Research",
-                    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
-        ext_modules=[mod_ext]
-        )
-
-
-# pypi
-# >>> python setup.py sdist bdist_wheel
-# >>> python -m twine upload dist/*
-# >>> python -m twine upload --repository testpypi dist/*
+    setup(ext_modules=[mod_ext])
+    
