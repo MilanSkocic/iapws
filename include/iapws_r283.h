201501db5952fde29b5129e2f3dbd45ac9174ca7
Milan Skocic <milan.skocic@icloud.com>
2023-11-27 11:44:31 +0100

    BLD: Fix missing extern declaration for header r283.

--- include/iapws_r283.h
+++ include/iapws_r283.h
@@ -6,19 +6,19 @@
 #ifndef IAPWS_R283_H
 #define IAPWS_R283_H
 
-#if _MSC_VER || __MINGW32__ || __MINGW64__
+#if _MSC_VER
 #define ADD_IMPORT __declspec(dllimport)
 #else
 #define ADD_IMPORT
 #endif
 
-ADD_IMPORT const double iapws_r283_capi_Tc_H2O; 
-ADD_IMPORT const double iapws_r283_capi_Tc_D2O;
+ADD_IMPORT extern const double iapws_r283_capi_Tc_H2O; 
+ADD_IMPORT extern const double iapws_r283_capi_Tc_D2O;
 
-ADD_IMPORT const double iapws_r283_capi_pc_H2O;
-ADD_IMPORT const double iapws_r283_capi_pc_D2O;
+ADD_IMPORT extern const double iapws_r283_capi_pc_H2O;
+ADD_IMPORT extern const double iapws_r283_capi_pc_D2O;
 
-ADD_IMPORT const double iapws_r283_capi_rhoc_H2O;
-ADD_IMPORT const double iapws_r283_capi_rhoc_D2O;
+ADD_IMPORT extern const double iapws_r283_capi_rhoc_H2O;
+ADD_IMPORT extern const double iapws_r283_capi_rhoc_D2O;
 
 #endif
