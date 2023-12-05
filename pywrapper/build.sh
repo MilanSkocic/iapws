fd6e320757e292564b6bf1fc786d54e479ac0500
Milan Skocic <milan.skocic@icloud.com>
2023-12-05 07:36:18 +0100

    BLD: Generic Makefiles in subdirectories.

--- pywrapper/build.sh
+++ pywrapper/build.sh
@@ -10,25 +10,26 @@ fi
 rm -r build
 rm -r dist
 rm -r *egg-info
-rm ./pyiapws/g704*.so
-rm ./pyiapws/r283*.so
-
-py311 setup.py sdist
 
 py38 setup.py build_ext --inplace
 py39 setup.py build_ext --inplace
 py310 setup.py build_ext --inplace
 py311 setup.py build_ext --inplace
+py312 setup.py build_ext --inplace
 
-py38 setup.py bdist_wheel
-py39 setup.py bdist_wheel
-py310 setup.py bdist_wheel
-py311 setup.py bdist_wheel
+py312 -m build --no-isolation --sdist
+
+py38 -m build --no-isolation --wheel
+py39 -m build --no-isolation --wheel
+py310 -m build --no-isolation --wheel
+py311 -m build --no-isolation --wheel
+py312 -m build --no-isolation --wheel
 
 py38 -m unittest -v
 py39 -m unittest -v
 py310 -m unittest -v
 py311 -m unittest -v
+py312 -m unittest -v
 
 if [[ "$OSTYPE" == "linux"* ]];then
     cd ./dist
