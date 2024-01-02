ccb867c04bcdf9f085109e22c435b0071b78f538
Milan Skocic <milan.skocic@icloud.com>
2024-01-02 20:16:56 +0100

    ENH: Add explicit public exports.

--- src/iapws_r283.f90
+++ src/iapws_r283.f90
@@ -3,6 +3,7 @@ module iapws__r283
     use iso_fortran_env
     use iso_c_binding
     implicit none
+    private
     
     real(real64), parameter :: Tc_H2O = 647.096d0 !! Critical temperature for H2O in K
     real(real64), parameter :: Tc_D2O = 643.847d0 !! Critical temperature for D2O in K
@@ -28,5 +29,7 @@ module iapws__r283
     real(c_double), protected, bind(C, name="iapws_rhoc_D2O") &
     :: capi_rhoc_D2O = rhoc_D2O !! Critical density for D2O in kg.m-3
     
+    public :: Tc_H2O, Tc_D2O, pc_H2O, pc_D2O, rhoc_H2O, rhoc_D2O
+    public :: capi_Tc_H2O, capi_Tc_D2O, capi_pc_H2O, capi_pc_D2O, capi_rhoc_H2O, capi_rhoc_D2O
     
 end module
