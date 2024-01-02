df6a36f053fda9e144d899d1e3f4ec75ca4e3859
Milan Skocic <milan.skocic@icloud.com>
2024-01-02 22:22:17 +0100

    MAINT: Remove unecessary windows specific code.

--- include/iapws_version.h
+++ include/iapws_version.h
@@ -5,11 +5,6 @@
 
 #ifndef iapws_VERSION_H
 #define iapws_VERSION_H
-#if _MSC_VER
-#define ADD_IMPORT __declspec(dllimport)
-#else
-#define ADD_IMPORT
-#endif
 extern char* iapws_get_version(void);
 #endif
 
