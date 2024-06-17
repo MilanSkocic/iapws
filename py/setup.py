4f8f11bf9ff02715a7224efed8da7fe8c0cb6bec
Milan Skocic <milan.skocic@icloud.com>
2024-06-17 11:13:43 +0200

    ENH: Refractoring setup.py.

--- py/setup.py
+++ py/setup.py
@@ -19,7 +19,7 @@ libraries = None
 library_dirs = None
 runtime_library_dirs = None
 extra_objects = None
-ext = None
+ext = [".so"]
 
 
 if platform.system() == "Linux":
@@ -78,25 +78,25 @@ files = LIBS
 for file in files:
     src = root_src / (file + ext[0])
     dest = root_dest / (file + ext[0])
+    cmd = ["install_name_tool", "-change", f"{str(src):s}", f"@loader_path/{src.name}", f"./src/py{name:s}/lib{name:s}{ext[0]:s}"]
     try:
         print(f"copying {str(src):s} -> {str(dest.parent):s}.")
         shutil.copy(src, dest)
         if platform.system() == "Darwin":
-            cmd = ["install_name_tool", "-change", f"{str(src):s}", f"@loader_path/{src.name}", f"./src/py{name:s}/lib{name:s}{ext[0]:s}"]
             print(cmd)
             subprocess.check_call(cmd)
     except subprocess.CalledProcessError:
-        print(cmd + " was not successful.")
+        print(" ".join(cmd) + " was not successful.")
     except:
         print(f"{file:s} was not found.")
 
 # check rpaths for Darwin
 if platform.system() == "Darwin":
+    cmd = ["otool", "-L", f"./src/py{name:s}/lib{name:s}{ext[0]:s}"]
     try:
-        cmd = ["otool", "-L", f"./src/py{name:s}/lib{name:s}{ext[0]:s}"]
         subprocess.check_call(cmd)
     except subprocess.CalledProcessError:
-        print(cmd + " was not successful.")
+        print(" ".join(cmd) + " was not successful.")
 
 version = None
 with open("./VERSION", "r") as f:
