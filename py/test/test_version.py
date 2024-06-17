c711937e191f58b869784908d1771623cc8541d3
Milan Skocic <milan.skocic@icloud.com>
2024-06-17 11:09:07 +0200

    TST: Automate test for version in python wrapper.

--- py/test/test_version.py
+++ py/test/test_version.py
@@ -2,11 +2,12 @@ r"""Tests for version."""
 import unittest
 from pyiapws.version import __version__
 
-
 class TestVersion(unittest.TestCase):
     r"""Test version module."""
 
     def test_version(self):
+        expected = None
+        with open("VERSION", "r") as f:
+            expected = f.read().strip()
         value = __version__
-        expected = "0.3.0"
         self.assertEqual(value, expected)
