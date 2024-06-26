ee25ff7780e17cd9917b951a7096370251366b9d
Milan Skocic <milan.skocic@icloud.com>
2024-06-26 17:05:33 +0200

    ENH: Add ni coefficients for region 4 and range of validity.

--- src/iapws_r797.f90
+++ src/iapws_r797.f90
@@ -3,6 +3,7 @@ module iapws__r797
     use iso_fortran_env
     use stdlib_kinds, only: dp
     use ieee_arithmetic
+    use iapws__r283, only: Tc_H2O, pc_H2O, rhoc_H2O
     implicit none
     private
 
@@ -10,16 +11,17 @@ module iapws__r797
 
     
 real(dp), parameter :: T_KELVIN = 273.15_dp !! Parameters from IAPWS R7-97
-real(dp), parameter ::  Tc = 647.096_dp !! critical temperature of water in K
-real(dp), parameter ::  pc = 22.064_dp !! critical pressure of the water in MPa
-real(dp), parameter :: rhoc = 322.0_dp !! critical density of the water in kg.m-3.
+real(dp), parameter ::  Tc = Tc_H2O !! critical temperature of water in K
+real(dp), parameter ::  pc = pc_H2O !! critical pressure of the water in MPa
+real(dp), parameter :: rhoc = rhoc_H2O !! critical density of the water in kg.m-3.
 
 real(dp), parameter :: R = 0.461526_dp !! Specific gas constant 0.461 526 kJ.kg-1.K-1
-    
-real(dp), parameter :: r1_ps = 16.53_dp
-real(dp), parameter :: r1_ts = 1386.0_dp
 
-!! IJn_r1f
+! Region1
+!-----------------------------------------------------------------------------
+real(dp), parameter :: r1_ps = 16.53_dp ! p*
+real(dp), parameter :: r1_ts = 1386.0_dp ! T*
+
 real(dp) :: IJn_r1f(9, 3) = transpose(reshape([&
 0.0_dp, -2.0_dp, 0.146_dp, &
 0.0_dp, -1.0_dp, -0.84548187169114_dp, &
@@ -30,9 +32,11 @@ real(dp) :: IJn_r1f(9, 3) = transpose(reshape([&
 0.0_dp, 4.0_dp, -0.16616417199501d-1, &
 0.0_dp, 5.0_dp, -0.81214629983568d-3, &
 1.0_dp, -9.0_dp, -0.28319080123804d-3], &
-[3, 9]))
+[3, 9])) !! Coefficients I, J and n for forward equation in region 1.
+!-----------------------------------------------------------------------------
 
-!! n_r4: ni coefficients for region 4 (saturation line)
+! Region 4
+!-----------------------------------------------------------------------------
 real(dp) :: n_r4(10) = [  &
 +0.11670521452767e4_dp,   &
 -0.72421316703206e6_dp,   &
@@ -44,7 +48,13 @@ real(dp) :: n_r4(10) = [  &
 +0.40511340542057e6_dp,   &
 -0.23855557567849_dp,     &
 +0.65017534844798e3_dp,   &
-])
+]) !! ni coefficients for region 4 (saturation line)
+
+real(dp), parameter :: r4_Tmin = 273.15_dp !! Lower bound for validity for ps(T) in K.
+real(dp), parameter :: r4_Tmax = 647.096_dp !! Upper bound for validity ps(T) in K.
+real(dp), parameter :: r4_pmin = 611.213e-3_dp !! Lower bound for validity for Ts(p) in MPa
+real(dp), parameter :: r4_pmax = 22.064_dp !! Upper bound for validity Ts(p) in MPa
+!-----------------------------------------------------------------------------
 
 contains
 
