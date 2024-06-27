7267902c8c34a9bfe0af510f2890faad11640e69
Milan Skocic <milan.skocic@icloud.com>
2024-06-27 06:58:37 +0200

    ENH: Start implementing r797.

--- src/iapws_r797.f90
+++ src/iapws_r797.f90
@@ -1,6 +1,5 @@
 module iapws__r797
     !! Module for IAPWS R7-97: Not fully implemented - under development.
-    use iso_fortran_env
     use stdlib_kinds, only: dp
     use ieee_arithmetic
     use iapws__r283, only: Tc_H2O, pc_H2O, rhoc_H2O
@@ -17,8 +16,10 @@ real(dp), parameter :: rhoc = rhoc_H2O !! critical density of the water in kg.m-
 
 real(dp), parameter :: R = 0.461526_dp !! Specific gas constant 0.461 526 kJ.kg-1.K-1
 
-! Region1
-!-----------------------------------------------------------------------------
+
+
+! Region 1
+!--------------------------------------------------------------------------------------------------------------------------------
 real(dp), parameter :: r1_ps = 16.53_dp ! p*
 real(dp), parameter :: r1_ts = 1386.0_dp ! T*
 
@@ -33,11 +34,13 @@ real(dp) :: IJn_r1f(9, 3) = transpose(reshape([&
 0.0_dp, 5.0_dp, -0.81214629983568d-3, &
 1.0_dp, -9.0_dp, -0.28319080123804d-3], &
 [3, 9])) !! Coefficients I, J and n for forward equation in region 1.
-!-----------------------------------------------------------------------------
+!--------------------------------------------------------------------------------------------------------------------------------
 
-! Region 4
-!-----------------------------------------------------------------------------
-real(dp) :: n_r4(10) = [  &
+
+
+! Region 4: Saturation line
+!--------------------------------------------------------------------------------------------------------------------------------
+real(dp) :: r4_n(10) = [  &
 +0.11670521452767e4_dp,   &
 -0.72421316703206e6_dp,   &
 -0.1707384694009292e2_dp, &
@@ -47,17 +50,26 @@ real(dp) :: n_r4(10) = [  &
 -0.48232657361591e4_dp,   &
 +0.40511340542057e6_dp,   &
 -0.23855557567849_dp,     &
-+0.65017534844798e3_dp,   &
-]) !! ni coefficients for region 4 (saturation line)
++0.65017534844798e3_dp    &
+] !! ni coefficients for region 4 (saturation line)
 
 real(dp), parameter :: r4_Tmin = 273.15_dp !! Lower bound for validity for ps(T) in K.
 real(dp), parameter :: r4_Tmax = 647.096_dp !! Upper bound for validity ps(T) in K.
 real(dp), parameter :: r4_pmin = 611.213e-3_dp !! Lower bound for validity for Ts(p) in MPa
 real(dp), parameter :: r4_pmax = 22.064_dp !! Upper bound for validity Ts(p) in MPa
-!-----------------------------------------------------------------------------
+
+real(dp), parameter :: r4_Tstar = 1_dp !! K
+real(dp), parameter :: r4_Pstar = 1_dp !! MPa
+!--------------------------------------------------------------------------------------------------------------------------------
+
+
+public :: psat, Tsat 
 
 contains
 
+
+! Region 1
+!--------------------------------------------------------------------------------------------------------------------------------
 pure elemental function gamma_(P, T)result(res)
     implicit none
 
@@ -103,6 +115,59 @@ pure elemental function iapws_r797_v(P, T)result(res)
     res = R*(T+T_KELVIN)/P * gamma_pi(P,T)
     
 end function
+!--------------------------------------------------------------------------------------------------------------------------------
+
+
+
+! Region 4: Saturation line
+!--------------------------------------------------------------------------------------------------------------------------------
+pure elemental function r4_ps(Ts)result(value)
+    !! Compute the saturation-pressure line.
+    real(dp), intent(in) :: Ts    !! Saturation temperature in °C.
+    real(dp) :: value !! Saturation pressure in MPa at temperature T.
+
+    real(dp) :: theta, Ts_K, A, B, C
+
+    Ts_K = (Ts + T_KELVIN) / r4_Tstar
+    theta = Ts_K + r4_n(9) / (Ts_K - r4_n(10))
+
+    A = theta**2           + r4_n(1) * theta + r4_n(2)
+    B = r4_n(3) * theta**2 + r4_n(4) * theta + r4_n(5)
+    C = r4_n(6) * theta**2 + r4_n(7) * theta + r4_n(8)
+
+    value = r4_Pstar  *( 2*C /(-B +(B**2-4*A*C)**(0.5_dp)))**(4.0_dp)
+end function
+
+pure elemental function r4_Ts(ps)result(value)
+    !! Compute the saturation-pressure line.
+
+    real(dp), intent(in) :: ps    !! Saturation pressure in MPa.
+    real(dp) :: value !! Saturation temperature in °C at pressure ps.
+
+    value = 1.0_dp     
+end function
+
+pure subroutine psat(Ts, ps)
+    !! Compute the saturation pressure at temperature Ts.
+
+    real(dp), intent(in) :: Ts(:) !! Saturation temperature in °C
+    real(dp), intent(out) :: ps(:)   !! Saturation pressure in MPa.
+
+    ps = r4_ps(Ts)
+
+end subroutine
+
+pure subroutine Tsat(ps, Ts)
+    !! Compute the saturation pressure at temperature Ts.
+
+    real(dp), intent(in) :: ps(:) !! Saturation temperature in °C
+    real(dp), intent(out) :: Ts(:)   !! Saturation pressure in MPa.
+
+    Ts = r4_Ts(ps)
+
+end subroutine
+
+!--------------------------------------------------------------------------------------------------------------------------------
 
 
 end module
