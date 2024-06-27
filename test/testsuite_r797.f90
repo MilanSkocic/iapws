b5dfd883a640d6297c8dfcb7cff22c09801fcf95
Milan Skocic <milan.skocic@icloud.com>
2024-06-27 12:16:22 +0200

    TST: Update test for r797 r4.

--- test/testsuite_r797.f90
+++ test/testsuite_r797.f90
@@ -11,24 +11,61 @@ contains
 
 subroutine collect_suite_r797(testsuite)
     type(unittest_type), allocatable, intent(out) :: testsuite(:)
-    testsuite = [new_unittest("Psat", test_Psat)]
+    testsuite = [new_unittest("Psat", test_Psat), new_unittest("Tsat", test_Tsat)]
 end subroutine
 
 subroutine test_Psat(error)
     type(error_type), allocatable, intent(out) :: error 
-    integer(int32) :: i, actual, expected
+    integer(int32) :: i
     real(dp) :: ps(3)
-    real(dp) :: Ts(3) = [300_dp, 500_dp, 600_dp]
+    real(dp) :: Ts(3) = [300.0_dp, 500.0_dp, 600.0_dp]
     real(dp) :: psref(3) = [0.353658941e-2_dp, 0.263889776e1_dp, 0.123443146e2_dp]
     real(dp) :: th(3) = [1e-11_dp, 1e-8_dp, 1e-7_dp]
     
     call psat(Ts-273.15_dp, ps)
     
+    ! check ref values from Table 35.
     do i=1, size(Ts)
         call check(error, ps(i), psref(i), thr=th(i))
         if (allocated(error)) return
     end do
+
+    ! Check out of range Ts.
+    Ts(1) = 273.0_dp
+    Ts(2) = 648.0_dp
+    call psat(Ts-273.15, ps)
+    do i=1, 2
+        call check(error, isnan(ps(1)), .true.)
+        if (allocated(error)) return
+    enddo
+
 end subroutine
 
+subroutine test_Tsat(error)
+    type(error_type), allocatable, intent(out) :: error 
+    integer(int32) :: i
+    real(dp) :: Ts(3)
+    real(dp) :: ps(3) = [0.1_dp, 1.0_dp, 10.0_dp]
+    real(dp) :: Tsref(3) = [0.372755919e3_dp, 0.453035632e3_dp, 0.584149488e3_dp]
+    real(dp) :: th(3) = [1e-6_dp, 1e-6_dp, 1e-6_dp]
+    
+    call Tsat(ps, Ts)
+    
+    ! check ref values from Table 36.
+    do i=1, size(Ts)
+        call check(error, Ts(i), Tsref(i), thr=th(i))
+        if (allocated(error)) return
+    end do
+
+    ! Check out of range ps.
+    Ts(1) = 0.611213_dp
+    Ts(2) = 22.064_dp
+    call Tsat(ps, Ts)
+    do i=1, 2
+        call check(error, isnan(ps(1)), .true.)
+        if (allocated(error)) return
+    enddo
+
+end subroutine
 
 end module
