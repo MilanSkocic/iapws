8826d296da8e2c7b667385a29cb8ee85bceeeaa4
Milan Skocic <milan.skocic@icloud.com>
2024-07-06 07:11:52 +0200

    Refractoring docstrings r797.

--- src/iapws_r797.f90
+++ src/iapws_r797.f90
@@ -10,11 +10,11 @@ module iapws__r797
 
     
 real(dp), parameter :: T_KELVIN = 273.15_dp !! Parameters from IAPWS R7-97
-real(dp), parameter ::  Tc = Tc_H2O !! critical temperature of water in K
-real(dp), parameter ::  pc = pc_H2O !! critical pressure of the water in MPa
-real(dp), parameter :: rhoc = rhoc_H2O !! critical density of the water in kg.m-3.
+real(dp), parameter :: Tc = Tc_H2O          !! critical temperature of water in K
+real(dp), parameter :: pc = pc_H2O          !! critical pressure of the water in MPa
+real(dp), parameter :: rhoc = rhoc_H2O      !! critical density of the water in kg.m-3.
 
-real(dp), parameter :: R = 0.461526_dp !! Specific gas constant 0.461 526 kJ.kg-1.K-1
+real(dp), parameter :: R = 0.461526_dp      !! Specific gas constant 0.461 526 kJ.kg-1.K-1
 
 
 
@@ -53,13 +53,13 @@ real(dp) :: r4_n(10) = [  &
 +0.65017534844798e3_dp    &
 ] !! ni coefficients for region 4 (saturation line)
 
-real(dp), parameter :: r4_Tmin = 273.15_dp !! Lower bound for validity for ps(T) in K.
-real(dp), parameter :: r4_Tmax = 647.096_dp !! Upper bound for validity ps(T) in K.
+real(dp), parameter :: r4_Tmin = 273.15_dp     !! Lower bound for validity for ps(T) in K.
+real(dp), parameter :: r4_Tmax = 647.096_dp    !! Upper bound for validity ps(T) in K.
 real(dp), parameter :: r4_pmin = 611.213e-6_dp !! Lower bound for validity for Ts(p) in MPa
-real(dp), parameter :: r4_pmax = 22.064_dp !! Upper bound for validity Ts(p) in MPa
+real(dp), parameter :: r4_pmax = 22.064_dp     !! Upper bound for validity Ts(p) in MPa
 
-real(dp), parameter :: r4_Tstar = 1_dp !! K
-real(dp), parameter :: r4_Pstar = 1_dp !! MPa
+real(dp), parameter :: r4_Tstar = 1_dp         !! K
+real(dp), parameter :: r4_Pstar = 1_dp         !! MPa
 !--------------------------------------------------------------------------------------------------------------------------------
 
 
