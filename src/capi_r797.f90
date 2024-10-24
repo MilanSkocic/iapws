099909f53876ec407c3d17c33be05e5a14b8bd24
Milan Skocic <milan.skocic@icloud.com>
2024-10-24 20:59:48 +0200

    Update docstrings for C API in R797.

--- src/capi_r797.f90
+++ src/capi_r797.f90
@@ -10,22 +10,26 @@ module iapws__capi_r797
 contains
 
 pure subroutine capi_psat(N, Ts, ps)bind(C, name="iapws_r797_psat")
+    !! C API. 
     !! Compute the saturation pressure at temperature Ts. 
+    !! Validity range 273.13 K <= Ts <= 647.096 K.
 
-    integer(c_size_t), intent(in), value :: N
-    real(c_double), intent(in) :: Ts(N)
-    real(c_double), intent(out) :: ps(N)
+    integer(c_size_t), intent(in), value :: N     !! Size of Ts and ps.
+    real(c_double), intent(in) :: Ts(N)           !! Saturation temperature in K.
+    real(c_double), intent(out) :: ps(N)          !! Saturation pressure in MPa. Filled with nan if out of validity range.
 
     call psat(Ts, ps)
 
 end subroutine
 
 pure subroutine capi_Tsat(N, ps, Ts)bind(C, name="iapws_r797_Tsat")
+    !! C API.
     !! Compute the saturation temperature at pressure ps.
+    !! Validity range 611.213 Pa <= ps <= 22.064 MPa.
     
-    integer(c_size_t), intent(in), value :: N
-    real(c_double), intent(in) ::   ps(N)
-    real(c_double), intent(out) ::    Ts(N)
+    integer(c_size_t), intent(in), value :: N     !! Size of ps and Ts.
+    real(c_double), intent(in) ::   ps(N)         !! Saturation pressure in MPa.
+    real(c_double), intent(out) ::  Ts(N)         !! Saturation temperature in K. Filled with nan if out of validity range.
     
     call Tsat(ps, Ts)
 
