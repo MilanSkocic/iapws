90be2b70b427498e55581c289ed7e67e162c3401
Milan Skocic <milan.skocic@icloud.com>
2024-10-24 21:10:51 +0200

    Update python wrapper R797
    Simplify C functions and update docstrings.

--- py/src/pyiapws/_r797.c
+++ py/src/pyiapws/_r797.c
@@ -7,16 +7,13 @@
 PyDoc_STRVAR(module_docstring, "C extension wrapping the iapws_r797 module of the Fortran iapws library.");
 
 PyDoc_STRVAR(r797_psat_doc, 
-"psat(Ts: array) --> mview \n\n"
+"psat(Ts: array-like) --> mview \n\n"
 "Get the saturation-pressure line. If gas not found returns NaNs");
 
 PyDoc_STRVAR(r797_Tsat_doc, 
-"Tsat(ps: array) --> mview \n\n"
+"Tsat(ps: array-like) --> mview \n\n"
 "Get the saturation-temperature line. If gas not found returns NaNs");
 
-static const char ERR_MSG_PARSING[] = "Ts is an object with the buffer protocol.";
-static const char ERR_MSG_T_DIM[] = "T must be a 1d-array of floats.";
-static const char ERR_MSG_T_TYPE[] = "T must be a 1d-array of floats.";
 
 static Py_buffer newbuffer_like(Py_buffer *buffer){
     Py_buffer newbuffer;
@@ -44,32 +41,16 @@ static PyObject *r797_psat(PyObject *self, PyObject *args){
     Py_buffer newbuffer;
     
     if(!PyArg_ParseTuple(args, "O", &T_obj)){
-        PyErr_SetString(PyExc_TypeError, ERR_MSG_PARSING);
         return NULL;
     }
-    if(PyObject_CheckBuffer(T_obj)==1){
-        mview = PyMemoryView_FromObject(T_obj);
-        buffer = PyMemoryView_GET_BUFFER(mview);
-        
-        if(strcmp(buffer->format, "d")!=0){
-            PyErr_SetString(PyExc_TypeError, ERR_MSG_T_TYPE);
-            return NULL;
-        }else if(buffer->ndim>1){
-            PyErr_SetString(PyExc_TypeError, ERR_MSG_T_DIM);
-            return NULL;
-        }else if(buffer->ndim==0){
-            PyErr_SetString(PyExc_TypeError, ERR_MSG_T_DIM);
-            return NULL;
-        }else{
-            newbuffer = newbuffer_like(buffer);
-            iapws_r797_psat(buffer->shape[0], (double *) buffer->buf, (double *) newbuffer.buf);
-            new_mview = PyMemoryView_FromBuffer(&newbuffer);
-            return new_mview;
-        }
-    }else{
-        PyErr_SetString(PyExc_TypeError, ERR_MSG_PARSING);
-        return NULL;
-    }
+
+    mview = PyMemoryView_FromObject(T_obj);
+    buffer = PyMemoryView_GET_BUFFER(mview);
+    newbuffer = newbuffer_like(buffer);
+    iapws_r797_psat(buffer->shape[0], (double *) buffer->buf, (double *) newbuffer.buf);
+    new_mview = PyMemoryView_FromBuffer(&newbuffer);
+
+    return new_mview;
 }
 
 static PyObject *r797_Tsat(PyObject *self, PyObject *args){
@@ -80,32 +61,16 @@ static PyObject *r797_Tsat(PyObject *self, PyObject *args){
     Py_buffer newbuffer;
     
     if(!PyArg_ParseTuple(args, "O", &p_obj)){
-        PyErr_SetString(PyExc_TypeError, ERR_MSG_PARSING);
         return NULL;
     }
-    if(PyObject_CheckBuffer(p_obj)==1){
-        mview = PyMemoryView_FromObject(p_obj);
-        buffer = PyMemoryView_GET_BUFFER(mview);
-        
-        if(strcmp(buffer->format, "d")!=0){
-            PyErr_SetString(PyExc_TypeError, ERR_MSG_T_TYPE);
-            return NULL;
-        }else if(buffer->ndim>1){
-            PyErr_SetString(PyExc_TypeError, ERR_MSG_T_DIM);
-            return NULL;
-        }else if(buffer->ndim==0){
-            PyErr_SetString(PyExc_TypeError, ERR_MSG_T_DIM);
-            return NULL;
-        }else{
-            newbuffer = newbuffer_like(buffer);
-            iapws_r797_Tsat(buffer->shape[0], (double *) buffer->buf, (double *) newbuffer.buf);
-            new_mview = PyMemoryView_FromBuffer(&newbuffer);
-            return new_mview;
-        }
-    }else{
-        PyErr_SetString(PyExc_TypeError, ERR_MSG_PARSING);
-        return NULL;
-    }
+
+    mview = PyMemoryView_FromObject(p_obj);
+    buffer = PyMemoryView_GET_BUFFER(mview);
+    newbuffer = newbuffer_like(buffer);
+    iapws_r797_Tsat(buffer->shape[0], (double *) buffer->buf, (double *) newbuffer.buf);
+    new_mview = PyMemoryView_FromBuffer(&newbuffer);
+
+    return new_mview;
 }
 
 
