457be5f1a058eb5c0a3fd684467ba55253c36ae2
Milan Skocic <milan.skocic@icloud.com>
2024-12-03 06:25:57 +0100

    Rewrite test for pytest.

--- py/test/test_version.py
+++ py/test/test_version.py
@@ -1,13 +1,9 @@
 r"""Tests for version."""
-import unittest
 from pyiapws.version import __version__
 
-class TestVersion(unittest.TestCase):
-    r"""Test version module."""
-
-    def test_version(self):
-        expected = None
-        with open("VERSION", "r") as f:
-            expected = f.read().strip()
-        value = __version__
-        self.assertEqual(value, expected)
+def test_version():
+    expected = None
+    with open("VERSION", "r") as f:
+        expected = f.read().strip()
+    value = __version__
+    assert value == expected
