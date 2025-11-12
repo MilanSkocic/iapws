dc84a0e27635426d0583f2154067c44f440ec7ef
Milan Skocic <milan.skocic@gmail.com>
2025-11-12 05:42:06 +0100

    Add new configure script.
    The script does not need to be sources anymore. It expors env vars in
    the make.in file that is generated on the fly.
    
    Add DESTDIR to be as close as possible from Makefiles generated by
    autotools.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.5.1-dev
+
+* Refactoring the `configure.sh` script.
+
 # 0.5.0
 
 * Remove support for Python 3.9 and add support for Python 3.14(t).
--- src/iapws_version.f90
+++ src/iapws_version.f90
@@ -2,5 +2,5 @@ module iapws__version
     !! Version
     implicit none
     private
-    character(len=*), parameter, public :: version = "0.5.0"
+    character(len=*), parameter, public :: version = "0.5.0dev0"
 end module iapws__version
