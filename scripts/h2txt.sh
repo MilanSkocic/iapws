9d0b63a8f982301237fda9e9875675486351c3eb
Milan Skocic <milan.skocic@gmail.com>
2026-03-22 23:37:50 +0100

    Add scripts for autodocs.

--- scripts/h2txt.sh
+++ scripts/h2txt.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+fpath="./srcprep/capi.txt"
 i=0
+
+echo "    C API:" > $fpath
 
 while IFS='' read -r lineraw; do
     line=$(echo $lineraw | sed -E 's/^[[:space:]]*//')
     case $line in
-        *"extern"*";"*)
-            echo "        o $(echo $line | sed -E 's/extern //g;s/ADD_IMPORT //g;s/;//g')"
+        "extern"*";"*)
+            echo "        o $(echo $line | sed -E 's/extern //g;s/ADD_IMPORT //g;s/;//g')" >> $fpath
             ;;
         *)
             ;;
--- scripts/py2txt.sh
+++ scripts/py2txt.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+fpath="./srcprep/pyapi.txt"
 i=0
+
+echo "    Python API:" > $fpath
 
 while IFS='' read -r lineraw; do
     line=$(echo $lineraw | sed -E 's/^[[:space:]]*//')
     case $line in
         "def"*":"*)
-            echo "        o $(echo $line | sed -E 's/def //g;;s/:*$//g')"
+            if [[ $line != "def _"* ]] && [[ $line != "def main"* ]]; then
+                echo "        o $(echo $line | sed -E 's/def //g;;s/:*$//g')" >> $fpath
+            fi
             ;;
         *)
             ;;
