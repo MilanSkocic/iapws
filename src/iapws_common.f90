dfb9eea8b77450b9370eebc0e630c54429c31f11
Milan Skocic <milan.skocic@gmail.com>
2026-03-27 06:32:14 +0100

    Refactoring and cleaning.
    Move all iso_c_binding related declarations to the common module.
    Add C API for R2-83 in its own module.

--- src/iapws_common.f90
+++ src/iapws_common.f90
@@ -2,15 +2,12 @@ module iapws__common
 !! Common parameters
 use stdlib_kinds, only: int64, dp, int32, sp
 use stdlib_optval, only: optval
-use ieee_arithmetic, only: ieee_quiet_nan, ieee_value
+use, intrinsic :: ieee_arithmetic, only: ieee_quiet_nan, ieee_value
+use, intrinsic :: iso_c_binding, only: c_double, c_int, c_ptr, c_f_pointer, &
+                  c_char, c_size_t, c_null_char, c_loc
 implicit none(type,external)
-private
+public
 
-!=======================================================================
-! PUBLIC
-!=======================================================================
-public :: optval
-public :: sp, dp, int32, int64
-public :: ieee_value, ieee_quiet_nan
-!=======================================================================
+real(dp), parameter ::  T_KELVIN = 273.15_dp !! Absolute temperature in KELVIN 
+
 end module iapws__common
